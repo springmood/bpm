98ba2465ca0818cb61683c9b7248b8198c2ea2f4
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-27 23:52:13 +0330

    add update,detail and delete of project to url

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('projects', views.list_view, name="projects"),
     path('project/create', views.project_store, name="project-create"),
     path('project/<str:pk>/', views.project, name="project"),
+    path('project/<id>',views.detail_view),
+    path('project/update/<id>',views.update_view,name="project-update"),
+    path('project/delete/<id>',views.delete_view,name="project-delete"),
 ]
