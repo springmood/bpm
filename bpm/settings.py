db2194c89c140240dcf0c58167fc1336a04d9186
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2022-12-27 22:10:56 +0330

    some CORS stuff added - still not working

--- bpm/settings.py
+++ bpm/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
