053368e0e15857da1882f1f6f3046754c3f3ea49
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-28 03:39:53 +0330

    get all task and create,delete,update and show special task to view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404,redirect
-from django.http import HttpResponse,JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from core.models import Project
-from .forms import ProjectForm
-from django.core import serializers
-
+from core.models import Project,Task
+from .forms import ProjectForm,TaskForm
 
 @csrf_exempt
 def project_store(request):
@@ -49,7 +47,7 @@ def project_update(request,id):
         return redirect("/project/"+id) 
     else:
         return JsonResponse({
-            "error" : "data is not fount"
+            "error" : "data is not found"
         }, safe=False)
 
 
@@ -66,6 +64,65 @@ def project_delete(request,id):
     })
 
 
+# task api
+@csrf_exempt
+def task_store(request):
+    this_title=request.POST['title']
+    this_desc=request.POST['desc']
+    this_type=request.POST['type']
+
+    Task.objects.create(title=this_title,desc=this_desc,type=this_type)
+    return JsonResponse({
+        "status":"created successful",
+    })
+
+
+@csrf_exempt
+def task_list(request):
+
+    context=Task.objects.all()
+    return JsonResponse({
+        "data" : list(context.values())
+    }, safe=False)
+
+
+
+@csrf_exempt
+def task_detail(request,id):
+
+    context=Task.objects.filter(id=id)
+    return JsonResponse({
+        "data" : list(context.values())
+    }, safe=False)
+
+
+
+@csrf_exempt
+def task_update(request,id):
+
+    obj=get_object_or_404(Task,id=id)
+    form=TaskForm(request.POST or None,instance=obj)
+
+    if form.is_valid():
+        form.save()
+        return redirect("/task/"+id) 
+    else:
+        return JsonResponse({
+            "error" : "data is not found"
+        }, safe=False)
+
+
+
+@csrf_exempt
+def task_delete(request,id):
+    obj=get_object_or_404(Task,id=id)
+
+    if request.method=="POST":
+        obj.delete()
+
+    return JsonResponse({
+        "status":"deleted successful",
+    })
 
     
 
