86af1a25397508c43fb3f8ed4c1c8968be821278
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-01-24 16:01:13 +0330

    replace url project member

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
 from project import views
 
-urlpatterns=[
-    path('projects', views.getAll, name="projects"),
-    path('project/create', views.store, name="project-create"),
-    path('project/<id>', views.detail),
-    path('project/update/<id>', views.update, name="project-update"),
-    path('project/delete/<id>', views.delete, name="project-delete"),
-
-
+urlpatterns = [
     # ----------------- project member
     path('project/members', views.project_member_list, name="project-members"),
     path('project/member/create', views.project_member_store,
@@ -18,5 +11,11 @@ urlpatterns=[
          name="project-member-delete"),
     path('project/member/update/<id>', views.project_member_update,
          name="project-member-update"),
+    # ------------------- project
+    path('projects', views.getAll, name="projects"),
+    path('project/create', views.store, name="project-create"),
+    path('project/<id>', views.detail),
+    path('project/update/<id>', views.update, name="project-update"),
+    path('project/delete/<id>', views.delete, name="project-delete"),
 
 ]
